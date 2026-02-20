9ae55a6b57711f3877f3a5a2f4fcf1d158665d1d
pravat ranjan senapati <pravatranjansenapati@gmail.com>
2026-02-20 14:27:09 +0530

    reome the comment line code update next time

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -88,52 +88,8 @@ const Login = async (req,res)=>{
     }
 }
 
-//change password
-
-// const changepassword = async (req,res)=> {
-//     try {
-//         const userid = req.userinfo.userid
-//         console.log(userid)
-//         const{oldPassword , newPassword} = req.body
-
-//         const user = await model.findById(userid)
-
-//         if(!user){
-//             return res.status(404).json({
-//                 success:false,
-//                 message:"User not found"
-//             })
-//         }
-
-//         const isMatch =await bcrypt.compare(oldPassword,user.password)
-//         if(!isMatch){
-//             return res.status(400).json({
-//                 success:false,
-//                 message:"old password is in correct"
-//             })
-//         }
-
-//         // hashpassword
-//         const salt = await bcrypt.genSalt(10)
-//         user.password= await bcrypt.hash(newPassword,salt)
-//         await user.save()
-
-//         res.status(200).json({
-//             success:true,
-//             message:"password change sucessfully"
-//         })
-
-
-//     } catch (error) {
-//         console.log(error)
-//         res.status(400).json({
-//             message:"Error you can't change the  password"
-//         })
-//     }
-// }
 
 module.exports = {
     register,
     Login,
-    // changepassword
 }
