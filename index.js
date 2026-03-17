ff0a77f509e0bb8b48a8ec0f17052e48843873f0
pravat ranjan senapati <pravatranjansenapati@gmail.com>
2026-03-17 11:13:21 +0530

    implement rate limiter

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const db = require('./config/db')
 const express = require('express')
 const app = express()
+const limit = require('express-rate-limit')
+
+
 db
 app.use(express.json())
 
+const limiter = limit({
+    windowMs: 10 * 60 * 1000,
+	limit: 50
+})
 
 app.get("/",(req,res)=>{
     res.send("server ready to serve ")
@@ -17,6 +24,7 @@ const image = require("./route/image.route")
 app.use("/user",user)
 app.use("/upload",image)
 
+app.use(limiter)
 app.listen(3000,()=>{
     console.log("server start")
 })
